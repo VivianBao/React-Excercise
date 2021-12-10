9e60341f226e0957a013e6b5a2adb6f18a26efde
Vivian Bow <vivianbao1009@gmail.com>
2021-12-10 21:23:48 +0900

    Created component files, Defined App functional component

---
+++ workshop/src/components/Gif.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gif = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Gif;
--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,9 @@
 import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+console.log('Hello!');
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
