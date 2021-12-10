7f11932640ff574516ce0f1c54259d36ec0de363
Vivian Bow <vivianbao1009@gmail.com>
2021-12-10 21:44:58 +0900

    test

---
+++ workshop/src/components/GifList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Gif from './Gif';
+
+const GifList = (props) => {
+  const { gifIDs } = props;
+  return (
+    <div className="gif-list">
+      {gifIDs.map((gifID) => <Gif gifID={gifID} />)}
+    </div>
+  );
+};
+
+export default GifList;
+// test
